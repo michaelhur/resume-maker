70ee196b7eb0c03e1faf31347a76fd1c96f4c1c1
jhhur <jhhur716@gmail.com>
2023-03-13 11:59:51 +0900

    refactor: 루트 컴포넌트 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { lazy, Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { TabMenu } from '@components/TabMenu/TabMenu';
 import { Header } from '@components/Header/Header';
@@ -16,26 +15,22 @@ const Blog = lazy(() => import('@pages/Blog'));
 function App() {
     const queryClient = getClient();
     return (
-        <>
-            <RecoilRoot>
-                <QueryClientProvider client={queryClient}>
-                    <Main>
-                        <Header />
-                        <Suspense fallback={<></>}>
-                            <Router>
-                                <TabMenu menus={PROFILE_TAB_MENU} size={'large'} position={'all'} />
-                                <Routes>
-                                    <Route path="/blog" element={<Blog />} />
-                                    <Route path="/profile" element={<Profile />} />
-                                    <Route path="/portfolio" element={<Portfolio />} />
-                                </Routes>
-                            </Router>
-                        </Suspense>
-                    </Main>
-                    <Footer />
-                </QueryClientProvider>
-            </RecoilRoot>
-        </>
+        <QueryClientProvider client={queryClient}>
+            <Main>
+                <Header />
+                <Suspense fallback={<></>}>
+                    <Router>
+                        <TabMenu menus={PROFILE_TAB_MENU} size={'large'} position={'all'} />
+                        <Routes>
+                            <Route path="/blog" element={<Blog />} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="/portfolio" element={<Portfolio />} />
+                        </Routes>
+                    </Router>
+                </Suspense>
+            </Main>
+            <Footer />
+        </QueryClientProvider>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Global } from '@emotion/react';
 import { globalStyle } from '@styles/globalStyle';
+import { RecoilRoot } from 'recoil';
 
 if (process.env.NODE_ENV === 'development') {
     import('./mocks');
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <>
+    <RecoilRoot>
         <Global styles={globalStyle} />
         <App />
-    </>,
+    </RecoilRoot>,
 );
